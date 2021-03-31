b83e13552594de60eb470fee8e93c31dc6fd59eb
devparkk01 <dev.parkk01@gmail.com>
2021-03-31 15:36:59 +0530

    Added single number 2

--- problem Solving/bitwise/minFlips.cpp
+++ problem Solving/bitwise/minFlips.cpp
@@ -1,5 +1,51 @@
 /*
 https://leetcode.com/problems/minimum-flips-to-make-a-or-b-equal-to-c/
 
+time of both approaches is O(32) which is same as 0(1)
+
 */
 
+// here , we are modifying a, b , c
+int minFlips(int a, int b, int c) {
+	// need to extract last bit from each of a  ,b , c
+	int x , y ,  z , f  = 1 ; int flipscount = 0 ;
+
+	for (int i = 0 ; i < 32 ; ++i ) {
+		x = (a & f ) ;
+		y = (b & f ) ;
+		z = (c & f ) ;
+		a = a >> 1 ; b = b >> 1 ; c = c >> 1 ;
+		// flipping begins
+		if ( z == 1 ) {
+			if ( x == 0 &&  y == 0 ) flipscount++ ;
+		}
+		else {
+			if ( x != 0 ) flipscount++ ;
+			if ( y != 0 ) flipscount++ ;
+		}
+	}
+	return flipscount ;
+}
+
+// here we aren't modifying a, b , c
+int minFlips(int a, int b, int c) {
+	// need to extract ith bit from each of a  ,b , c
+	int x , y ,  z ; int flipscount = 0 ;
+	// x , y , z stores ith bit of a, b and c respectively
+
+	for (int i = 0 ; i < 32 ; ++i ) {
+		int f = 1 ; f = f << i ;
+		x = (a & f ) ;
+		y = (b & f ) ;
+		z = (c & f ) ;
+		// flipping begins
+		if ( z  ) { // if z is set
+			if ( x == 0 &&  y == 0 ) flipscount++ ;
+		}
+		else { // z is not set
+			if ( x != 0 ) flipscount++ ;
+			if ( y != 0 ) flipscount++ ;
+		}
+	}
+	return flipscount ;
+}
--- problem Solving/bitwise/singelNumber2.cpp
+++ problem Solving/bitwise/singelNumber2.cpp
@@ -1,3 +1,49 @@
 /* https://leetcode.com/problems/single-number-ii/
 
+Given an integer array nums where every element appears three times except for one, which appears
+exactly once. Find the single element and return it.
+
 */
+
+/*  approach 1 : sort the array , and find out the element occuring once
+time : O(n logn  + n ) , space : O(1)
+*/
+
+
+/* appraoch 2 : count the frequency of each element and store it in a hashmap
+time : O(n) , space : O(n)
+*/
+int singleNumber(vector<int>& nums) {
+	unordered_map<int , int > ump ;
+	for (int &x : nums ) {
+		ump[x]++ ;
+	}
+	// for(auto it = ump.begin() ; it != ump.end() ; ++it ) {
+	// 	if ( it->second == 1 ) return it->first ;
+	// }
+	for (auto &it : ump) {
+		if ( it.second == 1 )return it.first ;
+	}
+	return -1 ;
+}
+
+/* approach 3 :
+
+
+
+*/
+int singleNumber(vector<int>& nums) {
+	int ans = 0 ;
+	int c ;
+	for (int  i = 0 ; i < 32 ; ++i  ) {
+		unsigned  f = 1 << i ; // try taking it unsigned
+		c = 0 ;
+		for (int &x : nums ) {
+			if ( (x & f ) > 0 ) {// ith bit is set in x
+				c++  ;
+			}
+		}
+		if (c % 3 == 1 ) ans += f ;
+	}
+	return ans ;
+}
