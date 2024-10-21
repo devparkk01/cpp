59aeae30717f7105789e2ba09a7dbbbd302440c5
devparkk01 <dev.parkk01@gmail.com>
2024-10-21 20:52:48 +0530

    Updates wordBreak dp problem

--- Algorithms/Dp/wordbreak.cpp
+++ Algorithms/Dp/wordbreak.cpp
@@ -1,7 +1,10 @@
 /*
-Given a string 'A' and a dictionary 'dictWords' , find out if it is possible to segment 'A' into one or
-many words given that these words must be present in the dictionary .
+139: 
+https://leetcode.com/problems/word-break/
 
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into 
+a space-separated sequence of one or more dictionary words.
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
 */
 
 
@@ -9,65 +12,87 @@ many words given that these words must be present in the dictionary .
 using namespace  std ;
 
 
-// recursive approach
+/*recursive approach
+    Time Complexity : O(2^N), Given a string of length n, there are n+1 ways to split it into two parts. At each
+    step, we have a choice: to split or not to split. In the worse case, when all choices are to be checked, that
+    results in O(2^N).
 
-// bool wordBreak(string A , vector<string>&dictWords ) {
-// 	int n = A.length() ;
-// 	if ( n == 0 )  return 1 ;
-// 	for (int i = 1 ; i <= n ; ++i) {
-// 		string prefix = A.substr(0 , i ) ;
-// 		if ( find(dictWords.begin() , dictWords.end() , prefix ) != dictWords.end()) {
-// 			if (wordBreak(A.substr(i) , dictWords) == 1 ) return 1 ;
-// 		}
-// 	}
-// 	return 0 ;
-// }
+*/
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size() ; 
+        unordered_set<string>dict(wordDict.begin(), wordDict.end());
+        return util(s, n , dict, 0);
+    }
+
+    // Returns true if it's possible to segment substring starting from index `start` till end, into space separated sequence of one or more dictionary words
+    bool util(string s, int n, unordered_set<string>&dict, int start) {
+		// if we have reached the end of the string, then it's possible to segment, return true 
+        if ( start == n) {
+            return true;
+        }
+
+        for(int i = start ; i < n ; ++i) {
+            string prefix = s.substr(start, i - start + 1);
+            if (dict.find(prefix) != dict.end() && util(s, n, dict, i + 1)) {
+                return true; 
+            }
+        }
+        return false; 
+    }
+};
+
+/* top down approach
+
+Time: O(N^3 + M), where N  is length of string s, where M is length of wordDict.
+There are total N dp states, they are dp[0], dp[1],.., dp[n], each dp state needs a loop O(N^2) to calculate the result.
+Plus the time to build hashset from wordDict, which is O(M)
+So total complexity is: O(N * N^2 + M) = O(N^3 + M)
+*/ 
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size() ; 
+        unordered_set<string>dict(wordDict.begin(), wordDict.end());
+		// mem[i] stores if it's possible to segment substring starting from index i till end, into space separated sequence of one or more dictionary words
+        vector<int>mem(n , -1);
+        return util(s, n , 0, dict, mem);
+    }
+
+    bool util(string s, int n, int start, unordered_set<string>&dict, vector<int>&mem) {
+		// if we have reached the end of the string, then it's possible to segment, return true 
+        if ( start == n) {
+            return true;
+        }
+        if ( mem[start] != -1) {
+            return mem[start];
+        }
+        for(int i = start ; i < n ; ++i) {
+            string prefix = s.substr(start, i - start + 1);
+            if (dict.find(prefix) != dict.end() && util(s, n, i + 1, dict, mem)) {
+                mem[start] = true; 
+                return mem[start];
+            }
+        }
+        mem[start] = false ; 
+        return mem[start];
+    }
+};
 
 
-// top down approach
-
-
-bool ifbreak(string A , unordered_set<string>&dict , unordered_map<string , int >&dp) {
-	int n = A.length() ;
-	if ( n == 0 ) return 1 ;
-	if ( dp.find(A) != dp.end()) return dp[A] ;
-
-	for (int i = 1 ; i <= n ; ++i) {
-		string prefix = A.substr(0 , i) ;
-		if ( dict.find(prefix) != dict.end()) {
-			if (ifbreak(A.substr(i) , dict , dp)) {
-				dp[A] = 1 ;
-				return dp[A] ;
-			}
-		}
-	}
-	dp[A] = 0 ;
-	return dp[A] ;
-
-}
-
-int wordBreak(string A, vector<string> &B) {
-	int n = A.length() ;
-	unordered_set<string> dict ;
-	unordered_map<string , int>dp  ;
-	for (string &x : B) dict.insert(x) ;
-
-	return ifbreak(A , dict , dp ) ;
-}
-
-
-
-int main()
-{
-
-	vector<string> dictWords = { "this", "th", "is", "famous", "Word", "break",
+int main() {
+	string s = "Wordbreakproblem";
+	vector<string> wordDict = { "this", "th", "is", "famous", "Word", "break",
 	                             "b", "r", "e", "a", "k", "br", "bre", "brea", "ak", "problem"
 	                           };
 
-	// input string
-	string A = "Wordbreakproblem";
+    
+    Solution test = Solution();
 
-	if (wordBreak(A , dictWords)) {
+	if (test.wordBreak(s , wordDict)) {
 		cout << "The string can be segmented";
 	}
 	else {
